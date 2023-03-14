3d52a888db88425814d7e231111fd8550c7bf143
Cjessiel <62605235+Cjessiel@users.noreply.github.com>
2023-03-14 10:20:44 -0300

    Desenvolvimento de busca e detalhamento de associado

--- src/main/java/br/com/api/assembleia/controller/AssociadoController.java
+++ src/main/java/br/com/api/assembleia/controller/AssociadoController.java
@@ -2,6 +2,7 @@ package br.com.api.assembleia.controller;
 
 import br.com.api.assembleia.dto.associado.DadosCadastroAssociadoDto;
 import br.com.api.assembleia.dto.associado.DadosListagemAssociadoDto;
+import br.com.api.assembleia.service.associado.BuscarAssociadoPorIdService;
 import br.com.api.assembleia.service.associado.CadastrarAssociadoService;
 import br.com.api.assembleia.service.associado.ListarAssociadoService;
 import org.springframework.data.domain.Page;
@@ -18,10 +19,14 @@ public class AssociadoController {
 
     private final ListarAssociadoService listarAssociadoService;
 
+    private final BuscarAssociadoPorIdService buscarAssociadoPorIdService;
+
     public AssociadoController(CadastrarAssociadoService cadastrarAssociadoService,
-                               ListarAssociadoService listarAssociadoService){
+                               ListarAssociadoService listarAssociadoService,
+                               BuscarAssociadoPorIdService buscarAssociadoPorIdService){
         this.cadastrarAssociadoService = cadastrarAssociadoService;
         this.listarAssociadoService = listarAssociadoService;
+        this.buscarAssociadoPorIdService = buscarAssociadoPorIdService;
     }
 
     @PostMapping
@@ -32,5 +37,10 @@ public class AssociadoController {
     @GetMapping
     public ResponseEntity<Page<DadosListagemAssociadoDto>> listar (@PageableDefault(size = 10, sort = {"id"}) Pageable paginacao){
         return listarAssociadoService.listar(paginacao);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity buscarPorId(@PathVariable Long id){
+        return buscarAssociadoPorIdService.buscarPorId(id);
     }
 }
